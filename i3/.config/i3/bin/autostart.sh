6f81c0a3cef39a8cd5741518b0a625d007cace4c
mech-keyboard69420 <keyboard69420@protonmail.com>
2022-08-26 06:42:55 +0300

    fix picom problem

--- i3/.config/i3/bin/autostart.sh
+++ i3/.config/i3/bin/autostart.sh
@@ -41,7 +41,7 @@ dunst -config $HOME/.config/i3/dunstrc &
 xfce4-power-manager &
 
 while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
-picom --experimental-backends --config $HOME/.config/i3/picom.conf &
+picom --config $HOME/.config/i3/picom.conf &
 
 # start polkit
 if [[ ! `pidof xfce-polkit` ]]; then
@@ -59,3 +59,7 @@ cp "$XFCE_TERM_PATH"/colorschemes/nord "$XFCE_TERM_PATH"/terminalrc
 CAVA_PATH="$HOME/.config/cava"
 cp "$CAVA_PATH"/colorschemes/nord "$CAVA_PATH"/config
 
+if [ ! $(pgrep "xidlehook") ]; then
+  notify-send "setting lock time"
+  xidlehook --not-when-fullscreen --timer 900 'alacritty -e cmatrix -C cyan' '' &
+fi
